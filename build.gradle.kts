80a5483112ae46ddef3f1978d5bca65f09820210
Rehan Julaha <rehan.julaha@kitaboo.com>
2026-07-13 18:51:51 +0530

    Add repositories to root build.gradle.kts for dependency resolution

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,18 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
     id("com.google.protobuf") version "0.9.4" apply false
 }
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
