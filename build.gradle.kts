ac2f6ff856a100579666714f8ae54fd5147c0525
Rehan Julaha <rehan.julaha@kitaboo.com>
2026-07-13 18:57:19 +0530

    Remove repositories from root build.gradle.kts (defined in settings.gradle)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,18 +4,3 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
     id("com.google.protobuf") version "0.9.4" apply false
 }
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
